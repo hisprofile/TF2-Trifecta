33232dc87700058a0d895ab363624e4c3cb69e13
hisanimations <41131633+hisprofile@users.noreply.github.com>
2022-11-13 14:19:25 +0100

    Add files via upload

---
+++ bonemerge.py
@@ -0,0 +1,172 @@
+import bpy
+
+loc = "BONEMERGE-ATTACH-LOC"
+rot = "BONEMERGE-ATTACH-ROT"
+
+bpy.types.Scene.target = bpy.props.PointerProperty(type=bpy.types.Object)
+def GetRoot(a):
+    for i in a:
+        if i.parent == None:
+            return i
+        
+class VIEW3D_PT_BONEMERGE(bpy.types.Panel):
+    """A Custom Panel in the Viewport Toolbar"""
+    bl_label = "Bonemerge"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Bonemerge"
+    bl_icon = "BONE_DATA"
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        
+        
+        row.label(text='Attach TF2 cosmetics.', icon='MOD_CLOTH')
+        ob = context.object
+        row = layout.row()
+        self.layout.prop_search(context.scene, "target", bpy.data, "objects", text="Link to", icon='ARMATURE_DATA')
+        
+        row = layout.row()
+        row.operator('hisanim.attachto', icon="LINKED")
+        row=layout.row()
+        row.operator('hisanim.detachfrom', icon="UNLINKED")
+        row = layout.row()
+        row.label(text='Bind facial cosmetics')
+        row = layout.row()
+        row.operator('hisanim.bindface')
+        row = layout.row()
+        row.label(text='Attempt to fix cosmetic')
+        row = layout.row()
+        row.operator('hisanim.attemptfix')
+        
+
+
+class HISANIM_OT_ATTACH(bpy.types.Operator):
+    bl_idname = "hisanim.attachto"
+    bl_label = "Attach"
+    bl_description = "Attach to a class"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        if context.scene.target == None:
+            raise TypeError("\n\nNo armature selected!")
+        obj = context.scene.target.name
+    
+        
+        for i in bpy.context.selected_objects:
+            if i == None:
+                continue
+            if not (i.type == 'ARMATURE' or i.type == 'MESH'):
+                continue # if the iteration is neither armature nor mesh, continue.
+            if i.name == obj:
+                continue # if the target is selected while cycling through selected objects, it will be skipped.
+            if i.type == 'MESH':
+                i = i.parent # if the mesh is selected instead of the parent armature, swap the iteration with its parent
+            for ii in i.pose.bones:
+                try:
+                    bpy.data.objects[obj].pose.bones[ii.name] # check if the target bone exists. if not, continue.
+                except:
+                    continue
+                
+                try:
+                    ii.constraints[loc] # check if constraints already exist. if so, swap targets. if not, create constraints.
+                    pass
+                except:
+                    ii.constraints.new('COPY_LOCATION').name = loc
+                    ii.constraints.new('COPY_ROTATION').name = rot
+                
+                ii.constraints[loc].target = bpy.data.objects[obj]
+                ii.constraints[loc].subtarget = ii.name
+                ii.constraints[rot].target = bpy.data.objects[obj]
+                ii.constraints[rot].subtarget = ii.name
+        
+        return {'FINISHED'}
+    
+class HISANIM_OT_DETACH(bpy.types.Operator):
+    bl_idname = "hisanim.detachfrom"
+    bl_label = "Detach"
+    bl_description = "Detach from a class"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        
+        for i in bpy.context.selected_objects:
+            if i == None:
+                continue
+            if not (i.type == 'ARMATURE' or i.type == 'MESH'):
+                continue
+            if i.type == 'MESH':
+                i = i.parent
+            for ii in i.pose.bones:
+                try:
+                    ii.constraints.remove(ii.constraints[loc])
+                    ii.constraints.remove(ii.constraints[rot])
+                except:
+                    continue
+        
+        return {'FINISHED'}
+class HISANIM_OT_BINDFACE(bpy.types.Operator):
+    bl_idname = 'hisanim.bindface'
+    bl_label = 'Bind Face Cosmetics'
+    bl_description = 'Bind facial cosmetics to a face'
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        #print('rad')
+        CON = context.selected_objects
+        
+        for i in CON[1].data.shape_keys.key_blocks:
+            find = i.name.casefold()
+            i.slider_min = -10
+            i.slider_max = 10
+            print(i)
+            for ii in CON[0].data.shape_keys.key_blocks:
+                if ii.name.casefold() == find:
+                    val = i.driver_add("value").driver
+                    val.variables.new()
+                    i.driver_add("value").driver.expression = 'var'
+                    val.variables[0].targets[0].id_type = 'KEY'
+                    val.variables[0].targets[0].id = CON[0].data.shape_keys
+                    val.variables[0].targets[0].data_path = f'key_blocks["{ii.name}"].value'
+            
+        
+        return {'FINISHED'}
+class HISANIM_OT_ATTEMPTFIX(bpy.types.Operator):
+    bl_idname = 'hisanim.attemptfix'
+    bl_label = 'Attempt to Fix Cosmetic'
+    bl_description = 'If a cosmetic appears to be worn incorrectly, this button may fix it'
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        SELECT = context.object
+        if not SELECT.type == 'ARMATURE':
+            SELECT = SELECT.parent
+        skipbone = SELECT.data.bones[0]
+        for i in SELECT.pose.bones:
+            if i.name == skipbone.name:
+                continue
+            try:
+                i.constraints[loc].enabled = False
+                i.constraints[rot].enabled = False
+            except:
+                pass
+        return {'FINISHED'}
+        
+'''classes = []
+classes.append(VIEW3D_PT_BONEMERGE)
+classes.append(HISANIM_OT_ATTACH)
+classes.append(HISANIM_OT_DETACH)
+classes.append(HISANIM_OT_BINDFACE)
+classes.append(HISANIM_OT_ATTEMPTFIX)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+    
+if __name__ == "__main__":
+    register()'''
---
+++ mercdeployer.py
@@ -0,0 +1,286 @@
+import bpy
+
+from pathlib import Path
+
+from bpy.props import BoolProperty
+
+prefs = bpy.context.preferences
+filepaths = prefs.filepaths
+asset_libraries = filepaths.asset_libraries
+blend_files = []
+
+global path
+global cln
+cln = ["IK", "FK"]
+path = [i.path for i in asset_libraries]
+
+for i in path:
+    if "TF2-V3" in i:
+        path = i.replace("\\", "/")
+        
+classes = ['scout', 'soldier', 'pyro', 'demo', 'heavy', 'engineer', 'medic', 'sniper', 'spy']
+def append(a, b): # get a class from TF2-V3
+    blendfile = f'{path}/{a}.blend'
+    section = "\\Collection\\"
+    object = a + b
+    
+    directory = blendfile + section
+    
+    bpy.ops.wm.append(filename=object, directory=directory)
+
+def appendtext(a): # add the .py script to add further control to faces
+    blendfile = f'{path}/{a}.blend'
+    section = "\\Text\\"
+    object = f'{a}.py'
+    
+    directory = blendfile + section
+    try:
+        bpy.data.texts[f'{a}.py']
+        bpy.data.texts[f'{a}.py'].use_fake_user = True
+    except:
+        try:
+            bpy.ops.wm.append(filename=object, directory=directory)
+        except:
+            return "cancelled"
+        try:
+            bpy.data.texts[f'{a}.py'].as_module()
+        except:
+            return "cancelled"
+        bpy.data.texts[f'{a}.py'].use_module = True
+        bpy.data.texts[f'{a}.py'].use_fake_user = True
+    return {'FINISHED'}
+
+
+def RemoveNodeGroups(a): # iterate through every node and node group by using the "tree" method and removing said nodes
+    for i in a.nodes:
+        if i.type == 'GROUP':
+            RemoveNodeGroups(i.node_tree)
+            i.node_tree.user_clear()
+            a.nodes.remove(i)
+        else:
+            a.nodes.remove(i)
+
+def NoUserNodeGroup(a): # remove fake users from node groups
+    for i in a.nodes:
+        if i.type == 'GROUP':
+            NoUserNodeGroup(i.node_tree)
+            i.node_tree.use_fake_user = False
+        else:
+            try:
+                i.use_fake_user = False
+            except:
+                pass
+                
+def PurgeNodeGroups(): # delete unused node groups from the .blend file
+    for i in bpy.data.node_groups:
+            if i.users == 0:
+                bpy.data.node_groups.remove(i)
+    return {'FINISHED'}
+
+def PurgeImages(): # delete unused images
+    for i in bpy.data.images:
+            if i.users == 0:
+                bpy.data.images.remove(i)
+    return {'FINISHED'}
+
+def SetActiveCol(a = None): # set the active collection
+    ihate = bpy.context.view_layer
+    if a == None:
+        ihate.active_layer_collection = ihate.layer_collection
+        return {'FINISHED'}
+def GetActiveCol(): # get the active collection
+    return bpy.context.view_layer.active_layer_collection
+
+def Collapse(a, b): # merge TF2 BVLG groups
+    if a.type == 'GROUP' and b in a.node_tree.name:
+        c = b + "-MD"
+        
+        if a.node_tree.name == c:
+            return "continue"
+        try:
+            bpy.data.node_groups[c]
+            a.node_tree = bpy.data.node_groups[c]
+            RemoveNodeGroups(bpy.data.node_groups[DELETE])
+        except:
+            a.node_tree.name = c
+            NoUserNodeGroup(a.node_tree)
+    
+    return {'FINISHED'}
+class VIEW3D_PT_MERCDEPLOY(bpy.types.Panel):
+    '''Rolling in the nonsense, deploy the fantasy!'''
+    bl_label = "Merc Deployer"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Merc Deployer"
+    bl_icon = "FORCE_DRAG"
+    
+    for i in classes:
+        global opname
+        opname = i
+        for ii in cln:
+            global opcln
+            opcln = ii
+            name = i+ii
+            class name(bpy.types.Operator):
+                merc = opname
+                type = opcln
+                bl_idname = f'''hisanim.{merc}{type.lower()}'''
+                bl_label = type
+                bl_options = {'UNDO'}
+                
+                def execute(self, context):
+                    bak = GetActiveCol()
+                    SetActiveCol()
+                    if appendtext(self.merc) == "cancelled":
+                        self.report({'INFO'}, "No Mercs Found!")
+                        return {'CANCELLED'}
+                    append(self.merc, self.type)
+                    justadded = str(self.merc + self.type) # make a variable targeting the added collection of the character
+                    matblacklist = []
+                    for obj in bpy.data.collections[justadded].objects: # iterate through collection of objects
+                        
+                        try:
+                            goto = bpy.data.collections['Deployed Mercs']
+                            goto.objects.link(obj)
+                            
+                        except:
+                            bpy.context.scene.collection.children.link(bpy.data.collections.new('Deployed Mercs'))
+                            goto = bpy.data.collections['Deployed Mercs']
+                            goto.objects.link(obj)
+                        # create new collection to contain all the mercs
+                        try:
+                            obj['COSMETIC']
+                            if context.scene.cosmeticcompatibility and obj['COSMETIC'] == False:
+                                bpy.data.objects.remove(obj)
+                                continue
+                            
+                            if not context.scene.cosmeticcompatibility and obj['COSMETIC']:
+                                bpy.data.objects.remove(obj)
+                                continue
+                        except:
+                            pass
+                        # remove non cosmetic compatible meshes, and vice versa.
+                        if obj.type == 'ARMATURE':
+                            armature = obj.name
+                            bpy.data.objects[armature].location = bpy.context.scene.cursor.location
+                            #move character to cursor
+                            continue
+                        
+                        for mat in obj.material_slots:
+                            mat = mat.material
+                            for NODE in mat.node_tree.nodes:
+                                
+                                #use existing nodegroups
+                                
+                                if Collapse(NODE, 'TF2 BVLG') == "continue":
+                                    continue
+                                
+                                
+                                
+                                if Collapse(NODE, 'TF2 Diffuse') == "continue":
+                                    continue
+                                
+                                
+                                
+                                if Collapse(NODE, 'TF2 Eye') == "continue":
+                                    continue
+                                
+                                #use existing images
+                                
+                                if NODE.type == 'TEX_IMAGE':
+                                    if ".0" in NODE.image.name:
+                                        try:
+                                            lookfor = NODE.image.name[:NODE.image.name.rindex(".")]
+                                            print(f'looking for {lookfor}..')
+                                            NODE.image = bpy.data.images[lookfor]
+                                            print("found!")
+                                            NODE.image.use_fake_user = False
+                                        except:
+                                            print(f"no original match found for {NODE.image.name}!")
+                                            print("RENAMING")
+                                            old = NODE.image.name
+                                            new = NODE.image.name[:NODE.image.name.rindex(".")]
+                                            print(f'{old} --> {new}')
+                                            NODE.image.name = new
+                                            NODE.image.use_fake_user = False
+                                            continue
+                                        
+                            if mat in matblacklist:
+                                continue
+                            # relevant towards BLU. if the material has already been swapped to BLU, continue.
+                            if context.scene.bluteam:
+                                foundred = False
+                                foundblu = False
+                                # all this to compensate for spy's head material.
+                                # multiple ifs are required, because if they were to be on the same line,
+                                # they would return an error.
+                                for NODE in mat.node_tree.nodes:
+                                    if NODE.type == 'TEX_IMAGE':
+                                        if 'blu' in NODE.image.name and foundblu == False:
+                                            foundblu = True
+                                            blutex = NODE
+                                            
+                                    if NODE.type == 'GROUP':
+                                        if 'blu' in NODE.name:
+                                            foundblu = True
+                                            blutex = NODE
+                                            
+                                    if NODE.type == 'TEX_IMAGE':
+                                        if 'red' in NODE.image.name and NODE.outputs[0].is_linked:
+                                            if NODE.outputs[0].links[0].to_node.type == 'GROUP':
+                                                getconnect = NODE.outputs[0].links[0].to_node
+                                                foundred = True
+                                                
+                                    if NODE.type == 'GROUP':
+                                        if 'red' in NODE.name and NODE.outputs[0].links[0].to_node.type == 'GROUP':
+                                            getconnect = NODE.outputs[0].links[0].to_node
+                                            foundred = True
+                                            
+                                    if foundred and foundblu: # if red and blu textures have been found, connect the blu node to TF2 Diffuse and append the current material to matblacklist.
+                                        mat.node_tree.links.new(blutex.outputs[0], getconnect.inputs[0])
+                                        matblacklist.append(mat)
+                                        break
+                    
+                    bpy.data.collections.remove(bpy.data.collections[justadded]) # remove the newly added collection.
+                    pending = []
+                    for i in bpy.data.objects[armature].pose.bones:
+                        # use existing bone shapes
+                        if i.custom_shape == None:
+                            continue
+                        shape = i.custom_shape.name
+                        
+                        if ".0" in shape:
+                            try:
+                                DELETE = shape
+                                if DELETE not in pending:
+                                    pending.append(DELETE)
+                                lookfor = shape[:shape.index(".")]
+                                i.custom_shape = bpy.data.objects[lookfor]
+                            except:
+                                bpy.data.objects[shape].name = shape[:shape.index(".")]
+                    print(pending)
+                    if len(pending) > 0:
+                        for i in pending:
+                            bpy.data.objects.remove(bpy.data.objects[i])
+                                    
+                                    
+                    print("DELETING")
+                    #delete  unused images and nodegroups.
+                    PurgeNodeGroups()
+                    PurgeImages()
+                    PurgeNodeGroups()
+                    bpy.context.view_layer.active_layer_collection = bak
+                    return {'FINISHED'}
+            bpy.utils.register_class(name)
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row(align=True)
+        for i in classes:
+            row.label(text=i)
+            col = layout.column()
+            for ii in cln:
+                row.operator(f'hisanim.{i}{ii.lower()}')
+            row = layout.row(align=True)
+        row.prop(context.scene, "bluteam")
+        row = layout.row()
+        row.prop(context.scene, "cosmeticcompatibility")
